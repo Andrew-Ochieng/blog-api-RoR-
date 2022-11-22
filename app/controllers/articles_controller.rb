ea70d042120646b55e84ce3305f2aa234415ba63
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-22 11:29:26 +0300

    add controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,43 @@ class ArticlesController < ApplicationController
     def index
         render json: Article.all
     end
+
+    def create
+        article = Article.create(article_params)
+        render json: article, status: :created
+
+    end
+
+    def show
+        article = Article.find_by(id: params[:id])
+        if article
+            render json: article
+        else
+            render json: {error: "Article not found"}, status: :not_found
+        end
+    end
+
+    def update
+        article = Article.find_by(id: params[:id])
+        if article
+            article.update(article_params)
+            render json: article
+        else
+            render json: {error: "Article not found"}, status: :not_found
+            
+        end
+    end
+
+    # def destroy
+    #     article = Article.find_by(id: params[:id])
+
+    #     if article
+    #         article.destroy
+    #     else
+    #         render json: {error: "Article deleted successfully"}, status: :
+    #     end
+    # end
+
+
+
 end
