5162a7ba6aa7bc351f4aeb59c9f27e80b95f1168
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-22 16:56:47 +0300

    add user basic validation to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,15 +29,22 @@ class ArticlesController < ApplicationController
         end
     end
 
-    # def destroy
-    #     article = Article.find_by(id: params[:id])
+    def destroy
+        article = Article.find_by(id: params[:id])
 
-    #     if article
-    #         article.destroy
-    #     else
-    #         render json: {error: "Article deleted successfully"}, status: :
-    #     end
-    # end
+        if article
+            article.destroy(article_params)
+        else
+            render json: {error: "Article deleted successfully"}, status: :no_content
+        end
+    end
+
+    # restrict access for params user can perfum CRUD operations on
+    private
+
+    def article_params
+        params.permit(:title, :body)
+    end
 
 
 
